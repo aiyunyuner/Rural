27b1cfd662df2d1f8b113f1483fd01b22ca5c138
TypeDelta <typedelta@outlook.com>
2023-02-21 00:23:39 +0800

    添加 config中心

---
+++ config-client/src/main/java/com/TypeDelta/controller/ConfigController.java
@@ -0,0 +1,16 @@
+package com.TypeDelta.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+    @Value("${name}")
+    private String name;
+
+    @GetMapping("/name")
+    public String getName() {
+        return name;
+    }
+}
